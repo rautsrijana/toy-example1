276c6d9d63f73fd5810df6ab71469f9444abb041
shhra <071bct538@pcampus.edu.np>
2020-05-20 17:40:09 +0545

    initial commit

---
+++ ml/src/data_loader.py
@@ -0,0 +1,118 @@
+import random
+import os
+
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+
+
+train_transformer = transforms.Compose([
+        transforms.Resize(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor()
+    ])
+
+eval_transformer = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor()
+    ])
+
+
+class BallotDataset(Dataset):
+    """
+    A standard PyTorch definition of Dataset which defines the functions __len__ and __getitem__.
+    """
+    def __init__(self, data_dir, data_desc, transform):
+        """
+        Store the filenames of the jpgs to use. Specifies transforms to apply on images.
+        Args:
+            data_dir: (string) directory containing the dataset
+            transform: (torchvision.transforms) transformation to apply on image
+        """
+        self.df = pd.read_csv(data_desc)
+        self.data_dir = data_dir
+        self.filenames, self.labels = self.fetch_file_and_label()
+        self.transform = transform
+
+    def fetch_file_and_label(self):
+        files = os.listdir(self.data_dir)
+        filenames = [os.path.join(self.data_dir, f)
+                for f in files if f.endswith('.jpeg')]
+        names = [x.split("/")[-1] for x in filenames]
+        labels = self.df[self.df['Data'].isin(names)]['Label'].tolist()
+        return filenames, labels
+
+    def __len__(self):
+        # return size of dataset
+        return len(self.filenames)
+
+    def __getitem__(self, idx):
+        """
+        Fetch index idx image and labels from dataset. Perform transforms on image.
+        Args:
+            idx: (int) index in [0, 1, ..., size_of_dataset-1]
+        Returns:
+            image: (Tensor) transformed image
+            label: (int) corresponding label of image
+        """
+        image = Image.open(self.filenames[idx])  # PIL image
+        image = self.transform(image)
+        return image, self.labels[idx]
+
+
+def fetch_dataloader(types, data_dir, params):
+    """
+    Fetches the DataLoader object for each type in types from data_dir.
+    Args:
+        types: (list) has one or more of 'train', 'val', 'test' depending on which data is required
+        data_dir: (string) directory containing the dataset
+        params: (Params) hyperparameters
+    Returns:
+        data: (dict) contains the DataLoader object for each type in types
+    """
+    dataloaders = {}
+
+    for split in ['train', 'val', 'test']:
+        if split in types:
+            path = os.path.join(data_dir, "{}_signs".format(split))
+
+            # use the train_transformer if training data, else use eval_transformer without random flip
+            if split == 'train':
+                dl = DataLoader(
+                        BallotDataset(path, 'data/train.csv', train_transformer), 
+                        batch_size=params.batch_size,
+                        shuffle=True,
+                        num_workers=params.num_workers,
+                        pin_memory=params.cuda)
+            elif split == 'val':
+                dl = DataLoader(
+                        BallotDataset(path, 'data/train.csv', eval_transformer), 
+                        batch_size=params.batch_size,
+                        shuffle=False,
+                        num_workers=params.num_workers,
+                        pin_memory=params.cuda)
+            else:
+                dl = DataLoader(
+                        BallotDataset(path, 'data/test.csv', eval_transformer), 
+                        batch_size=params.batch_size,
+                        shuffle=False,
+                        num_workers=params.num_workers,
+                        pin_memory=params.cuda)
+
+            dataloaders[split] = dl
+
+    return dataloaders
+
+
+if __name__ == '__main__':
+    dl = DataLoader(
+            BallotDataset('data/256x256_testset/train_signs', 'data/train.csv', train_transformer),
+            batch_size=4)
+
+    for i, x in enumerate(dl):
+        print(x)
+        if i == 1:
+            break
+
+    
---
+++ ml/src/train.py
@@ -0,0 +1,108 @@
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+import data_loader
+
+
+def train_model(model, criterion, optimizer, scheduler, dataloaders, num_epochs=10):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    since = time.time()
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+            size = len(dataloaders[phase])
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                inputs = Variable(inputs)
+                labels = Variable(labels)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+                print(running_loss, running_corrects)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / size
+            epoch_acc = running_corrects.double() / size
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model_ft = models.resnet18(pretrained=True)
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, 48)
+    model_ft = model_ft.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+    from utils import Params
+    params = Params("./ml/experiments/params.json")
+    data_dir = 'data/256x256_testset'
+    dataloaders = data_loader.fetch_dataloader(
+        ['train', 'val'], data_dir, params)
+    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                       dataloaders, num_epochs=10)
